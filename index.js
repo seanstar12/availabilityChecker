f447549eb276332e8781afae9de4d8168e5428b2
Sean <seanstar12@gmail.com>
2015-08-05 02:13:51 -0500

    Progress Update, can grab status code from reqest

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(8099)
+var express = require('express'),
+    fs = require('fs'),
+    http = require('http'),
+    AWS = require('aws-sdk');
+
+var app = express();
+var conf = getConfig();
+
+if (conf) {
+  app.get('/', function (req, res) {
+    res.send('Hello World ' + conf.port)
+  })
+  doRequest();
+  app.listen(conf.port);
+}
+
+function doRequest() {
+  var options = {
+    host: conf.host,
+    path: '/'
+  };
+
+  var req = http.request(options, function(res) {
+    console.log(options.host + ":" + res.statusCode);
+  });
+
+  req.on('error', function(err) {
+    console.log('Error during doRequest(), ' + err);
+  });
+
+  req.end();
+}
+
+function configExists() {
+  return fs.existsSync(__dirname + '/config');
+}
+
+function getConfig() {
+  console.log('Loading Config');
+
+  if (configExists()) {
+    return require('./config');
+  }
+  else { 
+    console.log('No config, creating one for you');
+    fs.createReadStream(__dirname + '/config.sample').pipe(fs.createWriteStream(__dirname + '/config'));
+    if (configExists()) {
+      return require('./config');
+    }
+    else {
+      console.log('Unable to create/read config.');
+      return false;
+    }
+  }
+}
